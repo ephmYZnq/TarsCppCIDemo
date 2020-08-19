dd7ec3206db2507eae02c6c183c3875efc38be25
ETZhangSX <etzhangsx@qq.com>
2020-08-19 20:56:31 +0800

    Add Counter singleton class

---
+++ HelloServer/src/Counter.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include "Counter.h"
+
+// using namespace tars;
+
+// Returns the current counter value, and increments it.
+int Counter::Increment() {
+  tars::TC_ThreadWLock wlock(_pRWLocker);
+  return counter_++;
+}
+
+// Returns the current counter value, and decrements it.
+// counter can not be less than 0, return 0 in this case
+int Counter::Decrement() {
+  if (counter_ == 0) {
+    tars::TC_ThreadRLock rlock(_pRWLocker);
+    return counter_;
+  } else  {
+    tars::TC_ThreadWLock wlock(_pRWLocker);
+    return counter_--;
+  }
+}
+
+// Prints the current counter value to STDOUT.
+int Counter::GetCount() {
+  tars::TC_ThreadRLock rlock(_pRWLocker);
+  return counter_;
+}
